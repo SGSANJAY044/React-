b940af888dc82697b3e256d5bf906654308ffccd
SGSANJAY044 <sgsanjay044@gmail.com>
2023-05-12 10:55:22 +0530

    react#1

--- router/src/Pages/Register.jsx
+++ router/src/Pages/Register.jsx
@@ -1,10 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 function Register(){
     const username=useRef(0);
     const password=useRef(0);
+    const [gender,setGender]=useState('')
+    const [skill,setSkill]=useState([])
+
     function handleSubmit(){
-        alert('Username: '+username.current.value+'\nPassword: '+password.current.value)
+        alert('Username: '+username.current.value+'\nPassword: '+password.current.value+'\nGender: '+gender+'\nSkill: '+skill)
     }
     return(
         <form onSubmit={handleSubmit}>
@@ -14,6 +17,18 @@ function Register(){
             Password
             <input type='text' ref={password}/>
             <br />
+            <div onChange={e=>setGender(e.target.value)}>
+                <input type="radio" name="gender" value="Male" />Male
+                <input type="radio" name="gender" value="Female" />Female
+            </div>
+            <div onChange={e=>setSkill((c)=>{
+                if(!c.includes(e.target.value))
+                    return([...c,e.target.value])
+                return c.filter(t=> t!=e.target.value)
+            })}>
+                <input type="checkbox" name="gender" value="Male" />Male
+                <input type="checkbox" name="gender" value="Female" />Female
+            </div>
             <input type="submit" value="submit"/>        
         </form>
     )
